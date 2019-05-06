93a4c42a15bf896fbd43a7ad11a0423481c884ac
atsutotsuruoka <atsuto.tsuruoka@di-v.co.jp>
2019-05-06 12:33:39 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "pictweet"
-set :repo_url, "git@example.atsutotsuruoka/pictweet.git"
+set :repo_url, "git@github.com:atsutotsuruoka/pictweet.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
